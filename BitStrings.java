cf2e09329f5b0b710b643caed54dc987d85503b9
semgoodd <ayoubsemgat20@gmail.com>
2025-09-11 20:21:55 +0200

    adding BitStrings exercise using modular exponentiation

--- BitStrings.java
+++ BitStrings.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+//using modular exponentiation
 public class BitStrings {
-    public static int bitStringss(int n){
-        return n;
+    static final long MOD = 1000000007;
+    public static long bitStrings(long base, long exp, long mod){
+        long a = 1;
+        base %= mod;
+        while (exp > 0){
+            if ((exp & 1) == 1){
+                a = (a * base) % mod;
+            }
+            base =(base * base) % mod;
+            exp >>= 1;
+        } 
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(bitStrings(2, n, MOD));
+        sc.close();
     }
 }
- 
+/*you can run it using only: 
+javac -d bin BitStrings.java
+java -cp bin BitStrings
+*/
